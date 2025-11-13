2cbacd546b069b82d5243d8a77688e81b9b72992
philG204 <philipp.gierenz@gmail.com>
2025-11-13 15:49:55 +0000

    Added MovieFactory template

--- UE06ActiveRecord/src/Factories/MovieFactory.java
+++ UE06ActiveRecord/src/Factories/MovieFactory.java
@@ -1,4 +1,30 @@
 package Factories;
 
+import db.DbManager;
+import Entities.Movie;
+import db.DbCredentials;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class MovieFactory {
+
+    private static DbManager conn;
+
+    public static Movie MovieFindById(long id){
+        String movieId_select = "SELECT ";
+        ResultSet rs = null;
+
+        conn.connectToDB();
+        //PreparedStatement db = conn.getConnection();
+        return null;
+
+    }
+
+    public static List<Movie> MovieFindByTitle(String title){
+        return null;
+    }
 }

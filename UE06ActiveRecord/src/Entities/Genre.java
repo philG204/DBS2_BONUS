a37edb92496c199d1f787fff63bcd9a95db7866e
Simon <Simon.Vogt@kabelmail.de>
2025-11-13 17:23:41 +0100

    Genre Klasse bearbeitet insert noch nicht fertig

--- UE06ActiveRecord/src/Entities/Genre.java
+++ UE06ActiveRecord/src/Entities/Genre.java
@@ -7,7 +7,6 @@ import db.*;
 
 public class Genre implements DbActions {
     private String genre;
-    private DbManager dbManager;
     private long genreID;
 
     public void setGenreID(long genreID) {
@@ -28,34 +27,20 @@ public class Genre implements DbActions {
 
     public int insert() throws SQLException {
         int num = 0;
+        int position1 = 1;
+        int position2 = 2;
         PreparedStatement stmt = null;
-        String getSequence = "SELECT nextval('genreid')";
-        String query = "INSERT INTO Genre VALUES ( ?, ? );";
+        String getSequence = "SELECT currval('genreid')";
+        String query = "INSERT INTO Genre VALUES ( nextval('genreid'), ? );";
         long sequence = 0;
-        dbManager =  DbManager.getInstance();
+        DbManager dbManager = DbManager.getInstance();
         dbManager.connectToDB();
-        try {
-            stmt = dbManager.getConnection().prepareStatement(getSequence);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                sequence = rs.getLong("genreID");
-            }
-            stmt.setLong(1, sequence);
-            stmt.setString( 2,genre);
-            num = stmt.executeUpdate();
-            System.out.println(num + " Zeile(n) geändert.");
-            setGenreID(sequence);
-
-        } catch (SQLException e) {
-            System.err.println("Fehler beim einfügen:\n" + e.getMessage());
-        }
-        try {
-            if (stmt != null) {
-                stmt.close();
-            }
-        } catch (SQLException e) {
-            System.out.println("Fehler beim Schließen des Statements: " + e.getMessage());
-        }
+        stmt = dbManager.getConnection().prepareStatement(query);
+        sequence = dbManager.getID(stmt);
+        dbManager.getConnection().prepareStatement(getSequence);
+        stmt.setLong(position1,sequence);
+        stmt.setString(position2,getGenre());
+        dbManager.executeInsert(stmt);
         return num;
     }
 }
